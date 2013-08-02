2b68d94595e15f10f92d715c4262bbcf634c1b32
kelubi <254810932@qq.com>
2013-08-02 16:14:45 +0800

    chang dir

--- update.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response
-import datetime
-
-def project_update(request):
-    servers = ('button1', 'button2', 'button3')
-    return render_to_response('update/list.html', {'servers': servers})
-
