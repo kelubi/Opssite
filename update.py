539a62db9b98cc5353e59c030f6d61a4891f37bd
kelubi <254810932@qq.com>
2013-08-02 18:29:39 +0800

    updadte

--- update.py
+++ update.py
@@ -6,11 +6,12 @@ def project_update(request,projectName):
 	if projectName not in project_list:
 		print "[ERROR] no %s project." % projectName
 		sys.exit()
-	servers = get_hosts()
+	servers = get_servers()
 	template_name = "update/%s.html" % projectName
 	return render_to_response(template_name, {'servers':servers})
 
+from django.db import models
 from opssite.models import VirtualServer
-def get_hosts():
-	hosts_list = VirtualServer.objects.all()
-	print hosts_list
+def get_servers():
+	servers_list = VirtualServer.objects.all()
+	print servers_list
