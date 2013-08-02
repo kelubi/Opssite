e3e821717ca5017e482c699335de74e3454b7472
kelubi <254810932@qq.com>
2013-08-02 17:16:14 +0800

    updadte

--- update.py
+++ update.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render_to_response
 import datetime
 
-def project_update(request):
-    servers = ('button1', 'button2', 'button3')
-    return render_to_response('update/list.html', {'servers': servers})
+def project_update(request,projectName):
+	project_list = ('bshare','lezhi','ads')
+	if projectName not in project_list:
+		print "[ERROR] no %s project." % projectName
+		sys.exit()
+    servers = ('button1','button2','button3')
+    phtml = "update/%s.html" % projectName
+    return render_to_response(phtml, {'servers':servers})
+
 
