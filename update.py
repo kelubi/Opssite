dd67f15811875a13c876ccb8c08f58453a5c7ade
kelubi <254810932@qq.com>
2013-08-05 12:20:21 +0800

    updadte

--- update.py
+++ update.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render_to_response
 import sys,datetime
 
+#from django.db import models
+#from ops_platform.models import Server
+#servers_list = VirtualServer.objects.all()
+def get_project():
+	from serverlists.projectlist import Projectlist
+	return Projectlist
+
+def get_servers(projectName):
+	impstr = 'from serverlists.%s import Hostlist,Applist' % projectName
+	exec(impstr)
+	return Hostlist,Applist
+
 def project_update(request,projectName):
-	project_list = ('bshare','lezhi','ads')
+	project_list = get_project()
 	if projectName not in project_list:
 		print "[ERROR] no %s project." % projectName
 		sys.exit()
-	servers = get_servers()
+	servers,app = get_servers(projectName)
 	template_name = "update/%s.html" % projectName
-	return render_to_response(template_name, {'servers':servers})
-
-#from django.db import models
-#from ops_platform.models import Server
-def get_servers():
-	#servers_list = VirtualServer.objects.all()
-	pass
+	return render_to_response(template_name, {'servers':servers,'app':app})
