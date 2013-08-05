98ec4a7af8dcd0b3116e01455c754248567ed38b
kelubi <254810932@qq.com>
2013-08-05 15:01:14 +0800

    updadte

--- update.py
+++ update.py
@@ -18,6 +18,6 @@ def project_update(request,projectName):
 	if projectName not in project_list:
 		print "[ERROR] no %s project." % projectName
 		sys.exit()
-	servers,app = get_servers(projectName)
+	servers,apps = get_servers(projectName)
 	template_name = "update/%s.html" % projectName
-	return render_to_response(template_name, {'servers':servers,'app':app})
+	return render_to_response(template_name, {'servers':servers,'apps':apps})
